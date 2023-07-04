d3fe86546393c8c43030c5238f87c860a7cc9dfe
Nolan Voss <nolan_voss@hotmail.com>
2023-07-04 01:34:00 -0400

    File uploads

--- src/Card.js
+++ src/Card.js
@@ -6,7 +6,7 @@ function Card (props) {
   return (
     <div>
       <Header profileImg = {props.commentObject.profileImg} username = {props.commentObject.username}/>
-      <Body comment = {props.comment}/>
+      <Body comment = {props.commentObject.comment}/>
     </div>
   );
 }
